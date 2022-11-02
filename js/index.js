4d1123b58b669e48bdd79d071709a3874a3900c3
Camilo <camiloeduardonn@ufps.edu.co>
2022-11-02 08:21:59 -0500

    fetch a tipycode

--- js/index.js
+++ js/index.js
@@ -3,8 +3,14 @@
 
 document.getElementById("boton").onclick = function(){
 
-fetch('http://localhost:3000/peticion', { mode: 'no-cors'})
+fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(json => console.log(json))
+}
 
-	}
+document.getElementById("boton2").onclick = function(){
+
+fetch('https://localhost:3000/peticion')
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
